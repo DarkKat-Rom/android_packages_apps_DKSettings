64fe0acfa73535e4c9a3e0a6d1569fbdb3c43565
XXMrHyde <xxmrhyde@gmx.co.uk>
2017-03-07 00:26:25 +0100

    Include needed changes for updated color picker library

--- src/net/darkkatrom/dksettings/SettingsColorPickerFragment.java
+++ src/net/darkkatrom/dksettings/SettingsColorPickerFragment.java
@@ -36,7 +36,7 @@ public class SettingsColorPickerFragment extends SettingsBaseFragment {
             if (extras != null && extras.getInt(extraNewColor) != 0) {
                 String extraPrefKey = ColorPickerPreference.PREFERENCE_KEY;
                 ((ColorPickerPreference) findPreference(extras.getString(extraPrefKey)))
-                        .setNewColor(extras.getInt(extraNewColor), true);
+                        .setNewColor(extras.getInt(extraNewColor));
             }
         }
     }
