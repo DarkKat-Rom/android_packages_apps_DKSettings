745e66733d62ea3e62d962a81b89a8f9d932487d
XXMrHyde <xxmrhyde@gmx.co.uk>
2018-06-17 22:55:52 +0200

    Theming: Accent color (part 5), (3/4):
    (1/4) : android_external_dkcolorpicker
    (2/4) : android_packages_apps_DKNotificationTest
    (3/4) : android_packages_apps_DKSettings
    (4/4) : android_packages_apps_DKWallpapers
    
    Fix color picker theme when activity was recreated (in general)

--- src/net/darkkatrom/dksettings/MainActivity.java
+++ src/net/darkkatrom/dksettings/MainActivity.java
@@ -166,62 +166,37 @@ public class MainActivity extends Activity implements
     }
 
     @Override
+    public boolean supportsTheming() {
+        return true;
+    }
+
+    @Override
     public int getThemeResId() {
-        return mThemeResId;
+        return R.style.AppTheme;
     }
 
     @Override
-    public boolean customizeColors() {
-        return mCustomizeColors;
+    public int getThemeLightActionBarResId() {
+        return R.style.AppTheme_LightActionBar;
     }
 
     @Override
-    public int getStatusBarColor() {
-        return mStatusBarColor;
+    public int getThemeLightStatusBarResId() {
+        return R.style.AppTheme_LightStatusBar;
     }
 
     @Override
-    public int getPrimaryColor() {
-        return mPrimaryColor;
+    public int getThemeLightNavigationBarResId() {
+        return R.style.AppTheme_LightNavigationBar;
     }
 
     @Override
-    public int getNavigationColor() {
-        return mNavigationColor;
+    public int getThemeLightActionBarLightNavigationBarResId() {
+        return R.style.AppTheme_LightActionBar_LightNavigationBar;
     }
 
     @Override
-    public boolean colorizeNavigationBar() {
-        return mColorizeNavigationBar;
-    }
-
-    @Override
-    public boolean lightStatusBar() {
-        return mLightStatusBar;
-    }
-
-    @Override
-    public boolean lightActionBar() {
-        return mLightActionBar;
-    }
-
-    @Override
-    public boolean lightNavigationBar() {
-        return mLightNavigationBar;
-    }
-
-    @Override
-    public boolean isWhiteoutTheme() {
-        return mIsWhiteoutTheme;
-    }
-
-    @Override
-    public boolean isBlackoutTheme() {
-        return mIsBlackoutTheme;
-    }
-
-    @Override
-    public int getThemeOverlayAccentResId() {
-        return mThemeOverlayAccentResId;
+    public int getThemeLightStatusBarLightNavigationBarResId() {
+        return R.style.AppTheme_LightStatusBar_LightNavigationBar;
     }
 }
